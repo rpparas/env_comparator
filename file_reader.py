478ef392a70537e07cfafe232d778e526a4865f4
Rommel <rpparas@gmail.com>
2021-05-05 10:02:02 -0700

    Update file reader with option to check size

--- file_reader.py
+++ file_reader.py
@@ -1,9 +1,22 @@
-def read_file_contents(filepath):
-    """ Open file and split newline delimeter """
+import os
+import logging
+
+def read_file_contents(filepath, file_size_limit=None):
+    """
+    Open file and split newline delimeter
+    If size is too big based on bytes specified, then output error message
+    """
     # TODO: optimize by introducing buffering (read as a streams)
     # TODO: get filesize first & warn if above threshold & confirm before loading into memory
     # TODO: identify if file is serializable (textual) and not binary (e.g. images)
 
+    if file_size_limit and get_file_size(filepath) > file_size_limit:
+        logging.error("File is too big to process")
+        exit(1)
+
     with open(filepath) as f:
         contents = f.readlines()
     return contents
+
+def get_file_size(filepath):
+    return os.path.getsize(filepath)
--- test_file_reader.py
+++ test_file_reader.py
@@ -10,3 +10,9 @@ def test_read_invalid_file_path():
     test_file = "invalid_path/.env"
     with pytest.raises(FileNotFoundError):
         read_file_contents(test_file)
+
+def test_big_env_file():
+    test_file = ".env"
+    with pytest.raises(SystemExit):
+        # For testing, assume that any file size greater than 1 is big
+        read_file_contents(test_file, file_size_limit=1)
